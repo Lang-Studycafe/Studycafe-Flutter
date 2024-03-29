97f6cd6be6333e4c24048ccd8d6e63a60451cbe1
hye-inA <hye.ina0130@gmail.com>
2024-03-29 09:52:36 +0900

    banner 생성

--- lib/studycafe2/render/mainPage/view/mainPageView1.dart
+++ lib/studycafe2/render/mainPage/view/mainPageView1.dart
@@ -10,6 +10,12 @@ class MainPageView1 extends StatefulWidget {
 class _MainPageView1State extends State<MainPageView1> {
 
   PageController pageController = PageController();
+  BannerModel bannerModel = BannerModel(
+    menus: [
+      BannerMenuModel(txt: "idx")],
+    items: [
+      BannerItemModel(img: "img", des: "des")]
+  );
 
   @override
   void dispose(){
@@ -21,36 +27,71 @@ class _MainPageView1State extends State<MainPageView1> {
   Widget build(BuildContext context) {
 
     // 배너, 이용권 화면
-    return Column(
-      children: [
-        SingleChildScrollView(
-          child: Column(
-            children: [
-              Container( //
-                color: Colors.grey,
-              )
-            ],
+    return Scaffold(
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Container( //
+                  child : banner(bannerModel),
+                )
+              ],
+            ),
           ),
-        ),
-        Container(
-          color: Colors.red,
-        )
+          Container(
+            color: Colors.red,
+          )
 
+        ],
+      ),
+    );
+  }
 
-      ],
+  Widget banner(BannerModel model) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            children:
+              model.menus.map<Widget>((BannerMenuModel menuModel) => TextButton(
+                onPressed: (){
+                  int index = model.menus.indexOf(menuModel);
+                  this.pageController.jumpToPage(index);
+                },
+                child: Text(menuModel.txt),
+                )).toList(),
+          ),
+          Container(
+            width: 300.0,
+            height: 300.0,
+            color: Colors.red,
+            child: PageView.builder(
+              controller: pageController,
+              itemCount: 3,
+              itemBuilder: (BuildContext context, int index) => Container(
+                child: Center(
+                  child: Text(model.items[index].des),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
 
 class BannerMenuModel {
-  final String idx; // 배너 index
+  final String txt; // 배너 index
   //const 생성자
-  const BannerMenuModel({required this.idx});
+  const BannerMenuModel({required this.txt});
 }
 
 class BannerItemModel {
   final String img;
-  const BannerItemModel({required this.img});
+  final String des;
+  const BannerItemModel({required this.img, required this.des});
 }
 
 class BannerModel {
